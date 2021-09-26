272fb0b8700b177541d166b8b53c0eee2f03e565
nassim1014 <heelnassim@gmail.com>
2021-09-26 21:58:42 +0100

    adding ML stuff vid 3

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import RandomForestRegressor
 from  sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+
 header=st.beta_container()
 dataset=st.beta_container()
 features=st.beta_container()
@@ -39,13 +40,20 @@ with modeltraining :
     sel_col,disp_col=st.beta_columns(2)
     max_depth=sel_col.slider("whats the max depth of the model",min_value=10,max_value=100,value=20,step=10)
     n_estimators=sel_col.selectbox('how many trees: ',options=[50,100,200,'No limits'],index=0)
+    sel_col.write(df.columns)
     input_feature=sel_col.text_input("what the hell","highway-mpg")
+
 
     regr=RandomForestRegressor(max_depth=max_depth,n_estimators=n_estimators)
     X=df[[input_feature]]
     Y=df[["price"]]
     regr.fit(X,Y)
     prediction=regr.predict(Y)
+
+    
+    fig=sns.regplot(x=input_feature,y="price",data=df)
+    fig.figure.savefig('fig.png')
+    st.image('fig.png')
 
     disp_col.subheader('MAE:')
     disp_col.write(mean_absolute_error(Y,prediction))
